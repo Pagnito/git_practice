a1849ff77b3d9019cec133f3d6efce9ca227b24d
Pavel <pavelyeganov@gmail.com>
2019-05-25 15:12:27 -0400

    m5

--- practice.js
+++ practice.js
@@ -6,13 +6,13 @@ let promise = new Promise((resolve, reject)=>{
 promise.then((word)=>{
   console.log(word)
 })
-function loop(num, words){
-  if(words.length!==num){
-    console.log('number and number of words dont match')
+function loop(num, fruits){
+  if(fruits.length!==num){
+    console.log('Number and number of words dont match')
     return;
   } else {
     for(let i = 0; i < num; i++){
-      console.log(words[i])
+      console.log(fruits[i])
     }
   } 
 }
